3bf51dd54df8b0dd82c11a6150b2ec05885f9e25
Ronish Shivakoti <ronishsiwakoti@gmail.com>
2024-05-02 18:46:57 -0400

    chore: Go router added for navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'bloc/theme_bloc.dart';
+import 'screen/category_screen/category_screen.dart';
 import 'screen/home_screen/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +18,23 @@ class MyApp extends StatelessWidget {
       create: (context) => ThemeBloc(),
       child: BlocBuilder<ThemeBloc, ThemeState>(
         builder: (context, state) {
-          return MaterialApp(theme: state.themeData, home: const HomeScreen());
+          return MaterialApp.router(
+              routerConfig: _router, theme: state.themeData);
         },
       ),
     );
   }
+
+  final GoRouter _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: "/",
+        builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        path: "/category",
+        builder: (context, state) => const CategoryScreen(),
+      )
+    ],
+  );
 }
