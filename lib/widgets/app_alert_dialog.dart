504a83ff5dc451373f9ab67177408f090422b672
Ronish Shivakoti <ronishsiwakoti@gmail.com>
2024-06-02 22:11:36 -0400

    fix:Logout dialog motified

--- lib/widgets/app_alert_dialog.dart
+++ lib/widgets/app_alert_dialog.dart
@@ -16,53 +16,72 @@ class AppAlertDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const titlePadding = EdgeInsets.only(top: 28);
+    const titlePadding = EdgeInsets.only(top: 28, left: 26);
     const contentPadding = EdgeInsets.symmetric(horizontal: 26, vertical: 19);
     const actionsPadding = EdgeInsets.only(bottom: 28);
 
     return AlertDialog(
       backgroundColor: AppColor.darkBackgroundColor,
-      surfaceTintColor: AppColor.searchColor,
       title: LabelText(
         text: title,
         size: 20,
         color: AppColor.primaryColor,
-        textAlign: TextAlign.center,
       ),
       shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(10))),
       titlePadding: titlePadding,
       contentPadding: contentPadding,
       actionsPadding: actionsPadding,
-      actionsAlignment: MainAxisAlignment.spaceEvenly,
+      actionsAlignment: MainAxisAlignment.end,
       content: LabelText(
         text: content,
         color: AppColor.primaryColor,
         fontWeight: FontWeight.w400,
         maxLine: 3,
-        textAlign: TextAlign.center,
       ),
       actions: <Widget>[
-        AppButton(
-          width: 76,
-          fontSize: 14,
-          fontWeight: FontWeight.w400,
-          ontap: () {
-            Navigator.pop(context, false);
-          },
-          text: "Cancel",
-          textColor: AppColor.textColor,
-          color: AppColor.primaryColor,
+        const DialogButton(
+          buttonText: 'Cancel',
+          borderColor: AppColor.headerColor,
         ),
-        AppButton(
-          width: 76,
-          ontap: onConfirm,
-          text: "Yes",
-          fontSize: 14,
-          fontWeight: FontWeight.w400,
-          color: AppColor.buttonColor,
+        DialogButton(
+          buttonText: 'Ok',
+          borderColor: AppColor.primaryColor,
+          buttonBgColor: AppColor.primaryColor,
         ),
       ],
+    );
+  }
+}
+
+class DialogButton extends StatelessWidget {
+  final Color borderColor;
+  final String buttonText;
+  final Color? buttonBgColor;
+  const DialogButton({
+    super.key,
+    required this.borderColor,
+    required this.buttonText,
+    this.buttonBgColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.pop(context, false);
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 7),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(color: borderColor, width: 2),
+            color: buttonBgColor ?? Colors.transparent),
+        child: LabelText(
+          text: buttonText,
+          size: 15,
+        ),
+      ),
     );
   }
 }
