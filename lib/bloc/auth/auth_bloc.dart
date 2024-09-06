f206b4f91f1ed89dd79fec4b91725d17482d27a5
Ronish Shivakoti <ronishsiwakoti@gmail.com>
2024-09-05 23:05:26 -0400

    feat: user model added

--- lib/bloc/auth/auth_bloc.dart
+++ lib/bloc/auth/auth_bloc.dart
@@ -1,8 +1,6 @@
-
 import 'package:bloc/bloc.dart';
 import 'package:himalayan_delights/bloc/auth/auth_event.dart';
 import 'package:himalayan_delights/bloc/auth/auth_state.dart';
-
 import '../../repositories/auth_repository.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
@@ -15,13 +13,13 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     if (event is AuthSignInRequested) {
       yield AuthInProgress();
       try {
-        final user = await _authRepository.signInWithGoogle();
+        final user = await authRepo.signInWithGoogle();
         yield AuthSuccess(user!);
       } catch (e) {
         yield AuthFailure(e.toString());
       }
     } else if (event is AuthSignOutRequested) {
-      await _authRepository.signOut();
+      await authRepo.signOut();
       yield AuthInitial();
     }
   }
