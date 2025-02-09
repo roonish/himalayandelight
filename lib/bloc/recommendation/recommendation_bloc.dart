8cea9f7b60f5fc181cf3b249de24aa267df71ff8
Ronish Shivakoti <ronishsiwakoti@gmail.com>
2025-02-08 23:02:44 -0500

    feat: recommendation repository added

--- lib/bloc/recommendation/recommendation_bloc.dart
+++ lib/bloc/recommendation/recommendation_bloc.dart
@@ -1,15 +1,15 @@
 import 'package:bloc/bloc.dart';
 import 'package:built_collection/built_collection.dart';
 import 'package:equatable/equatable.dart';
+import 'package:himalayan_delights/repositories/recm_repository.dart';
 
 import '../../model/recommendation.dart';
 
 part 'recommendation_event.dart';
 part 'recommendation_state.dart';
 
-class RecommendationBloc extends Bloc<RecommendationEvent, RecommendationState> {
-  final Rec
-  RecommendationBloc() : super(RecommendationInitial()) {
-  
-  }
+class RecommendationBloc
+    extends Bloc<RecommendationEvent, RecommendationState> {
+  final RecmRepository recmRepository;
+  RecommendationBloc(this.recmRepository) : super(RecommendationInitial()) {}
 }
